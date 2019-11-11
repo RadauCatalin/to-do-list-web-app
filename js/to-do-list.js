c446a39875c4d33566b9ee30be7fdefcd459f669
Catalin <catalin.radau@yahoo.com>
2019-11-11 20:53:16 +0200

    Added support for creating new items

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -13,7 +13,26 @@ window.ToDoList = {
             ToDoList.displayItems(JSON.parse(response));
         });
     },
+    createItem: function () {
+        let descriptionValue = $("#description-field").val();
+        let deadlineValue = $("#deadline-field").val();
 
+        var requestBody = {
+            description: descriptionValue,
+            deadline: deadlineValue
+        };
+
+        $.ajax({
+            url: ToDoList.API_URL,
+            method: "POST",
+            //MIME type
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+            ToDoList.getItems();
+        })
+
+    },
     displayItems: function (items) {
         var tableContent = "";
         items.forEach(item => tableContent += ToDoList.getItemTableRow(item));
@@ -34,7 +53,15 @@ window.ToDoList = {
             <td><a href="#" class="delete-item" data-id="${item.id}">
                 <i class="far fa-trash-alt"></i></a></td>
             </tr>`
-    }
+    },
+    bindEvents: function () {
 
+        $("#create-item-form").submit(function (event) {
+            event.preventDefault();
+
+            ToDoList.createItem();
+        })
+    }
 };
 ToDoList.getItems();
+ToDoList.bindEvents();
